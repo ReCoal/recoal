5741ee4e0e89935083cdaa375c1fbdb9b4a8e62e
ReCoal <recoalcommunity@gmail.com>
2018-05-19 00:37:43 +0530

    Update blockchain_blackball.cpp

--- src/blockchain_utilities/blockchain_blackball.cpp
+++ src/blockchain_utilities/blockchain_blackball.cpp
@@ -1,3 +1,4 @@
+// Copyright (c) 2018, ReCoal Project
 // Copyright (c) 2014-2018, The Monero Project
 //
 // All rights reserved.
@@ -178,7 +179,7 @@ int main(int argc, char* argv[])
     "database", available_dbs.c_str(), default_db_type
   };
   const command_line::arg_descriptor<bool> arg_rct_only  = {"rct-only", "Only work on ringCT outputs", false};
-  const command_line::arg_descriptor<std::vector<std::string> > arg_inputs = {"inputs", "Path to Monero DB, and path to any fork DBs"};
+  const command_line::arg_descriptor<std::vector<std::string> > arg_inputs = {"inputs", "Path to ReCoal DB, and path to any fork DBs"};
 
   command_line::add_arg(desc_cmd_sett, arg_blackball_db_dir);
   command_line::add_arg(desc_cmd_sett, cryptonote::arg_testnet_on);
@@ -208,12 +209,12 @@ int main(int argc, char* argv[])
 
   if (command_line::get_arg(vm, command_line::arg_help))
   {
-    std::cout << "Monero '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")" << ENDL << ENDL;
+    std::cout << "ReCoal '" << MONERO_RELEASE_NAME << "' (v" << MONERO_VERSION_FULL << ")" << ENDL << ENDL;
     std::cout << desc_options << std::endl;
     return 1;
   }
 
-  mlog_configure(mlog_get_default_log_path("monero-blockchain-blackball.log"), true);
+  mlog_configure(mlog_get_default_log_path("recoal-blockchain-blackball.log"), true);
   if (!command_line::is_arg_defaulted(vm, arg_log_level))
     mlog_set_log(command_line::get_arg(vm, arg_log_level).c_str());
   else
